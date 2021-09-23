7d5547b7298b16e06fbeaabb532399cfb27e9130
hsaigusa <hirokazu.k.saigusa@gmail.com>
2021-09-23 12:23:51 +1000

    within checker updated packages

--- geometry_within_checker.py
+++ geometry_within_checker.py
@@ -2,7 +2,8 @@
 
 # Import necessary packages
 from qgis.core import *
-import qgis.utils import iface
+from qgis.utils import iface
+from PyQt5.QtWidgets import *
 import time
 
 # Assign time to check run time
@@ -11,17 +12,12 @@ tmStart = time.time()
 # for pop-up window
 parent = iface.mainWindow()
 
-# Create combobox to select coutnries
-# Layers - from Natural Earth website (https://www.naturalearthdata.com/)
+# Layers - from Natural Earth website (
 layers = QgsProject.instance().mapLayersByName("Countries")
-
-# Check the validity of the data
 if len(layers) > 0:
     countries = layers[0]
     if countries.isValid():
         lCntry_names = []
-
-        # Assign filters
         request = QgsFeatureRequest()
         nmClause = QgsFeatureRequest.OrderByClause("NAME")
         orderby = QgsFeatureRequest.OrderBy([nmClause])
@@ -29,14 +25,12 @@ if len(layers) > 0:
         for cntry in countries.getFeatures(request):
             lCntry_names.append(cntry['NAME'])
 
-        # Extract geometries from country layer
         sCntry, bOk = QInputDialog.getItem(parent, "City Names", "Select country: ", lCntry_names)
         if bOk:
             for cntry in countries.getFeatures():
                 if cntry['NAME'] == sCntry:
                     geomCntry = cntry.geometry()
 
-            # City layers - create filter and compare geometry using within method
             layers = QgsProject.instance().mapLayersByName("ne_10m_populated_places")
             if len(layers) > 0:
                 cities = layers[0]
@@ -60,7 +54,6 @@ if len(layers) > 0:
 else:
     print("Countries was not found")
 
-# Check run time, and print out the list of cities within the selected country
 if bOk:
     tmEnd = time.time()
     sStr = f"Run time = {round((tmEnd - tmStart), 3)} seconds\n\n" + sStr
